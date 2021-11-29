d6d1ae4eb17281d9ccde63cc02262f1b0785a80b
hemant.hc <iamhemantchaudhary1@gmail.com>
2021-11-29 13:55:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import { Weather } from "./weather";
 
-function App() {
+const baseAPI = "https://api.openweathermap.org/data/2.5";
+const apiKey = "dcd1fdaeafc0bcfbfbb74aead52784dd";
+
+export default function App() {
+  const [city, setCity] = useState("");
+  const [data, setData] = useState([]);
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${baseAPI}/weather/?q=${city}&units=metric&APPID=${apiKey}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+          setCity("");
+        });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="search">
+        <input
+          className="search-box"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setCity(e.target.value)}
+          value={city}
+          onKeyPress={search}
+        />
+      </div>
+      <Weather data={data} />
     </div>
   );
 }
-
-export default App;
